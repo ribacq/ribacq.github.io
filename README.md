b070e4f3b9280b189d9f2ca93cdeaf12fe921916
Quentin RIBAC <ribac.quentin@gmail.com>
2017-03-21 00:32:50 +0100

    Improved MD and translation support

---
+++ README.md
@@ -0,0 +1,18 @@
+# ribacq.github.io
+
+## Français
+Ce site web est une présentation de moi-même, Quentin RIBAC, étudiant français en informatique, et
+de mes projets. Il est destiné à être parcouru rapidement, bilingue en français et anglais, et de
+présenter mes activités d’étudiant, de programmeur, et d’amateur de linguistique. Tout simplement,
+j’aime les langages, qu’ils soient de programmation, naturels, ou construits.
+
+## English
+This website is a presentation of myself, Quentin RIBAC, French Computer Sciences student, and of my
+projects. It is intended to be quick to read, bilingual English and French, and to present my
+activity as a student, programmer, and amateur linguistics fan. Really, I like languages, be they
+programming, natural, or constructed languages.
+
+## Technologies
+* HTML5/CSS3
+* JS?
+
--- data/bio.js
+++ data/bio.js
@@ -1,21 +1,42 @@
-bioData = {
-	"fr":`
-	# Quentin RIBAC
-	
-	Utilisateur de GNU/Linux depuis que j’ai 14 ans, je suis aujourd’hui un utilisateur
-	avancé de ce système d’exploitation et passionné de programmation.
-	
-	Je suis actuellement étudiant en première année de DUT Informatique à l’**IUT de Lannion**
-	(Côtes d’Armor), et compte poursuivre après ce diplôme des études d’ingénieur en informatique.
-	
-	Dans mon travail comme dans ma vie, j’apprécie la **rigueur**, l’**originalité**, la **clarté**,
-	et dans une certaine mesure le **minimalisme**.
-	J’exprime ceci par l’utilisation d’outils comme l’éditeur de texte *VI*, le gestionnaire
-	de fenêtres *dwm* ou la disposition de clavier *BÉPO*.
-	
-	L’algorithmique est l’aspect de l’informatique qui m’attire le plus aujourd’hui. Par
-	[mes réalisations](https://github.com/ribacq), je suis à la recherche de nouvelles connaissances
-	et, surtout, d’expérience.
-	`
+bio = {
+fr:`
+# Quentin RIBAC
+
+Utilisateur de GNU/Linux depuis que j’ai 14 ans, je suis aujourd’hui un utilisateur
+avancé de ce système d’exploitation et passionné de programmation.
+
+Je suis actuellement étudiant en première année de DUT Informatique à l’**IUT de Lannion**
+(Côtes d’Armor), et compte poursuivre après ce diplôme des études d’ingénieur en
+informatique.
+
+Dans mon travail comme dans ma vie, j’apprécie la **rigueur**, l’**originalité**, la
+**clarté**, et dans une certaine mesure le **minimalisme**.
+J’exprime ceci par l’utilisation d’outils comme l’éditeur de texte *VI*, le gestionnaire
+de fenêtres *dwm* ou la disposition de clavier *BÉPO*.
+
+L’algorithmique est l’aspect de l’informatique qui m’attire le plus aujourd’hui. Par
+[mes réalisations](https://github.com/ribacq), je suis à la recherche de nouvelles
+connaissances et, surtout, d’expérience.
+`,
+
+en:`
+# Quentin RIBAC
+
+GNU/Linux user since I am 14, I today am an advanced user of this
+operating system, and passioned with programming.
+
+I am currently a student in first year of a « DUT Informatique », a
+two-year Computing Sciences diploma, at the **IUT of Lannion** (Brittany).
+
+In my life as in personal life, I value **rigor**,
+**originality**, **clarity**, and to a certain
+extent **minimalism**. I’m applying this through the use of
+some tools like the text editor *VI*, the *dwm* windows
+manager, or the *BÉPO* French keyboard disposition.
+
+Algorithmics is the aspect of Computer Sciences attracting me the most
+today. Through [my realizations](https://github.com/ribacq),
+I am looking for more knowledge, and more than everything, experience.
+`
 };
 
