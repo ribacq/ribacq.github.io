36ab854c733431261b7bf293a3f48443f7b2bf27
Quentin RIBAC <ribac.quentin@gmail.com>
2017-04-02 12:43:29 +0200

    added next page button

--- js/main.js
+++ js/main.js
@@ -2,27 +2,37 @@
 var lang = 'fr';
 
 //list of pages
-var pages = {
-	bio: 'md',
-	skills: 'html',
-	projects: 'md',
-	perso: 'md',
-	contact: 'md'
-};
+var pages = ['bio', 'skills', 'projects', 'perso', 'contact'];
+var noMdPages = ['skills'];
 var homePage = 'bio';
 
 //load pages content
 function loadPage(pageName) {
-	var pageFormat = pages[pageName];
+	//get page format and location
+	var pageFormat = (noMdPages.indexOf(pageName) === -1) ? 'md' : 'html';
 	var uri = 'pages/' + lang + '/' + pageName + '.' + pageFormat;
 	var section = $('section');
+	
 	$.get(uri, {}, function (content) {
+		//display and parse markdown if necessary
 		section.html(content);
 		if (pageFormat === 'md') {
 			var article = $('article.' + lang + '.' + pageName);
 			article.html(markdown.toHTML(article.html(), 'Maruku'));
 		}
+
+		//next page link
+		if (pages.indexOf(pageName) !== (pages.length - 1)) {
+			section.append('<a href="#next">' + ((lang === 'fr') ? 'page suivante' : 'next page') + ' &rarr;</a>');
+			$('a[href="#next"]').click(function () {
+				loadPage(pages[pages.indexOf(pageName) + 1]);
+			});
+		}
 	}, 'text');
+
+	//update current in menu
+	$('nav a').removeClass('curMenu');
+	$('nav a[href="#' + pageName + '"]').addClass('curMenu');
 }
 loadPage(homePage);
 
@@ -30,8 +40,6 @@ loadPage(homePage);
 $('nav a').click(function (e) {
 	var tab = e.target.href.split('#');
 	var current = tab[tab.length - 1];
-	$('nav a').removeClass('curMenu');
-	$('nav a[href="#' + current + '"]').addClass('curMenu');
 	loadPage(current);
 });
 
